df61a632ebdae48a26d933426d6816377af754b7
Taylor Dickson <taylor.dickson@ontario.ca>
2025-08-22 11:11:11 -0400

    feat(Models): :sparkles: Create MergeTask Model
    Created a merge task model that stores the details of a merge, a user should be able to execute multiple tasks

---
+++ models.ts
@@ -0,0 +1,13 @@
+export interface MergeTask {
+	id: string;
+	name: string;
+	pluginVersion: string;
+	enabled: boolean;
+	eventTrigger: string; // dynamic, can be any event name
+	includePath: string;
+	savePath: string;
+	orderBy?: string | ((a: unknown, b: unknown) => number); // string or function for extensibility
+	orderDirection?: 'asc' | 'desc';
+	lastRun?: Date; // Optional, to track last run time
+	lastRunResult?: string; // Optional, to store the result of the last run
+}
